db7216ccba107899d1764bde03b7971a5d0e20a0
suraj <suraj@surajjosh7@gmail.com>
2024-02-24 23:28:50 +0530

    feat: multiple input on single command

--- index.js
+++ index.js
@@ -2,65 +2,79 @@
 const domainprobe=require('domainprobe');
 
 let select_functionality = process.argv[2];
-let input = process.argv[3];
-let input2=process.argv[4];
-if(select_functionality==='ip'){
-    domainprobe.get_ip(input,(err,data)=>{
+let input = process.argv.slice(3);
+
+if(select_functionality=='ip'){
+    for(let i=0;i<input.length;i++){
+    domainprobe.get_ip(input[i],(err,data)=>{
         if(err){
             console.error(err)
         }
+        console.log('ip of '+ input[i])
         console.table(data);
-    })
+    })}
 }
-else if(select_functionality==='hostname'){
-    domainprobe.get_hostname(input,(err,data)=>{
+else if(select_functionality=='hostname'){
+    for(let i=0;i<input.length;i++){
+    domainprobe.get_hostname(input[i],(err,data)=>{
         if(err){
             console.error(err)
         }
+        console.log("hostname of "+ input[i])
         console.table(data)
-    })
+    })}
 }
-else if(select_functionality==='domain-info'){
-    domainprobe.data_extract(input,(err,data)=>{
+else if(select_functionality=='domain-info'){
+    for(let i=0;i<input.length;i++){
+    domainprobe.data_extract(input[i],(err,data)=>{
         if(err){
             console.error(err)
         }
+        console.log("domain information of "+ input[i])
         console.table(data);
-    })
+    })}
 }
-else if(select_functionality==='ip-info'){
-    domainprobe.data_extract_ip(input,(err,data)=>{
+else if(select_functionality=='ip-info'){
+    for(let i=0;i<input.length;i++){
+    domainprobe.data_extract_ip(input[i],(err,data)=>{
         if(err){
             console.error(err)
         }
+        console.log("information of "+ input[i])
         console.table(data)
-    })
+    })}
 }
-else if(select_functionality==='location'){
-    domainprobe.exact_location(input,input2,(err,data)=>{
+else if(select_functionality=='location'){
+    for(let i=0;i<input.length;i+=2){
+    domainprobe.exact_location(input[i],input[i+1],(err,data)=>{
         if(err){
             console.error(err)
         }
+        console.log(`location of long ${input[i]} lat ${input[i+1]}`)
         console.table(data)
-    })
+    })}
 }
-else if(select_functionality==='mx'){
-    domainprobe.get_mx(input,(err,data)=>{
+else if(select_functionality=='mx'){
+    for(let i=0;i<input.length;i++){
+    domainprobe.get_mx(input[i],(err,data)=>{
         if(err){
             console.error(err)
         }
+        console.log("mx record of "+ input[i])
         console.log(data)
-    })
+    })}
 }
-else if(select_functionality==='cname'){
-    domainprobe.get_cname(input,(err,data)=>{
+else if(select_functionality=='cname'){
+    for(let i=0;i<input.length;i++){
+    domainprobe.get_cname(input[i],(err,data)=>{
         if(err){
             console.error(err)
         }
+        console.log("cname "+input[i])
         console.log(data)
-    })
+    })}
 }
-else if (select_functionality === '--help') {
+else if (select_functionality == '--help') {
     console.log(`
 Usage: domainprobe [operation] [input] [optional_input]
 
